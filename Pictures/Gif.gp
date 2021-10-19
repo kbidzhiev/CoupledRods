575ce91d30a6a67169a72aff87d360cb16699e09
kbidzhiev <bidzhiev.kemal@physics.msu.ru>
2021-10-19 16:23:53 +0200

    works

---
+++ Pictures/Gif.gp
@@ -0,0 +1,32 @@
+
+###########################################
+set terminal gif animate delay 5
+set output 'Oscilators.gif'
+FILE_LEFT = '../Data/Data.dat'
+
+stats FILE_LEFT nooutput
+
+
+set xrange [*:*]
+set yrange [-10: 10]
+set ylabel "Sx"
+set xlabel "x"
+percentile="P5 P10 P20 P25 P50 P75"
+cir=7;
+
+col1="black";
+col2="red";
+col3="orangel";
+col4="#04B431";
+col5="#3399FF";
+col6="dark-violet";
+
+
+f(x) = x;
+do for [i=1:int(STATS_blocks)-1] {
+    plot FILE_LEFT index (i) u ($1):($2) w lp pt cir ps 1.5 lt rgb "red" title columnheader 
+}
+
+!eog "./Oscilators.gif" &
+
+
--- src/Coupled_Rods.cpp
+++ src/Coupled_Rods.cpp
@@ -10,14 +10,14 @@
 
 #include <vector>
 #include <fstream>
-
+#include <cmath>
 using namespace std;
 
 
 //x''+x==0;
-const double L = 1;
+const double L = 11;
 const double T = 10;
-const double dt = 0.01;
+const double dt = 0.1;
 
 int main() {
 
@@ -29,17 +29,31 @@ int main() {
 	Data.precision(15);
 
 
-	vector<double> X(L,1);
-	vector<double> P(L,0);
+	vector<double> X(L,1.0);
+	for (size_t i = 0; i < X.size(); ++i){
+		double R = X.size();
+		double pos = i - R/2 + 0.5;
+
+		X[i] = sqrt(R*R/4 - pos*pos);
+	}
+	//X[X.size()/2] = 0.5;
+	vector<double> P(L,0.0);
 
 	const int N_steps = T/dt;
 	for (int n = 0; n < N_steps; ++n){
-		for (size_t i = 0; i < P.size(); ++i){
-			P[i] = P[i] + dt * X[i];
+		for (size_t i = 1; i < P.size() - 1; ++i){
 			X[i] = X[i] - dt * P[i];
+			P[i] = P[i] + dt * ( X[i+1] + 2*X[i] + X[i-1]);
 		}
-		for (auto elem : X){
-			Data << dt * n << "\t" << elem  << "\n";
+
+		if (true){
+			Data << "\"t = " << n*dt << "\"\n";
+			int position = 1;
+			for (auto elem : X){
+				Data << position++ << "\t" << elem  <<"\t"<< dt * n <<"\n";
+			}
+			Data <<"\n\n";
+
 		}
 		//cout << endl;
 	}
